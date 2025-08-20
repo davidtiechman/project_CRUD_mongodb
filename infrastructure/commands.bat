0d374232d96dc94e1113fdbc210224949a78df73
Knobloch Moshe <moskno1@gmail.com>
2025-08-20 13:10:01 +0300

    the project working

--- infrastructure/commands.bat
+++ infrastructure/commands.bat
@@ -4,3 +4,9 @@ docker run -p 8000:8000 davidnachmen/fastapi-mongo:latest
 
 docker push davidnachmen/fastapi-mongo:latest
 docker pull davidnachmen/fastapi-mongo:latest
+
+oc apply -f mongo-pvs.yaml
+oc apply -f mongodb-deploynent.yaml
+oc apply -f mongo-service.yaml
+oc apply -f fastapi-for-mongo.yaml
+
